1cb3e189638f7029e36a2922c3293f7c16bc5b2d
Amanda <52755024+Mellix@users.noreply.github.com>
2025-02-11 10:56:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "YIPPEEEE :D LUNCH AND MOVIE DATE WHEN YOU'RE OFF WORK?";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
